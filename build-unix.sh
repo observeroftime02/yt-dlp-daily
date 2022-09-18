3747a520983e2728b49db157fb97f0e8af0d7c85
observeroftime01 <57168924+observeroftime01@users.noreply.github.com>
2022-09-17 18:26:24 -0700

    [build] added Windows and Unix build scripts to the repo ci skip all

---
+++ build-unix.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+GREEN='\033[1;32m'
+ORANGE='\033[0;33m'
+NC='\033[0m'
+rm -rf ./build
+rm -rf ./dist
+rm ./yt-dlp
+printf "${GREEN}Installing dependencies${NC}\n"
+apt-get -y install zip pandoc man
+python3 -m pip install --upgrade pip setuptools wheel twine
+python3 -m pip install "https://observeroftime01.github.io/Pyinstaller-Builds/x86_64/pyinstaller-5.3-py3-none-any.whl" -r requirements.txt
+printf "${GREEN}Preparing for build${NC}\n"
+python3 devscripts/update-version.py
+python3 devscripts/make_lazy_extractors.py
+printf "${GREEN}Building the various packages${NC}\n"
+make all tar
+python3 pyinst.py --onedir
+(cd ./dist/yt-dlp_linux && zip -r ../yt-dlp_linux.zip .)
+python3 pyinst.py
+mv ./yt-dlp ./dist/yt-dlp
+printf "${GREEN}Creating checksums${NC}\n"
+sha256sum ./dist/yt-dlp >> ./dist/sha256sum
+sha256sum ./dist/yt-dlp_linux >> ./dist/sha256sum
+sha256sum ./dist/yt-dlp_linux.zip >> ./dist/sha256sum
+sha512sum ./dist/yt-dlp >> ./dist/sha512sum
+sha512sum ./dist/yt-dlp_linux >>./dist/sha512sum
+sha512sum ./dist/yt-dlp_linux.zip >> ./dist/sha512sum
+# uncomment and modify the paths below to have the script automatically update your local executable after build
+# printf "${GREEN}Updating local version${NC}\n"
+# rm /usr/local/bin/youtube-dl
+# cp ./dist/yt-dlp_linux /usr/local/bin/youtube-dl
+# chmod +x /usr/local/bin/youtube-dl
+printf "${ORANGE}All finished, all your executables are in the dist folder${NC}\n"
---
+++ build-windows.ps1
@@ -0,0 +1,25 @@
+Remove-Item -LiteralPath ".\dist\" -Force -Recurse
+python devscripts/update-version.py
+python devscripts/make_lazy_extractors.py
+python -m pip install --upgrade pip setuptools wheel py2exe
+pip install "https://observeroftime01.github.io/Pyinstaller-Builds/x86_64/pyinstaller-5.3-py3-none-any.whl" -r requirements.txt
+python setup.py py2exe
+Move-Item ./dist/yt-dlp.exe ./dist/yt-dlp_min.exe
+python pyinst.py
+python pyinst.py --onedir
+Compress-Archive -Path ./dist/yt-dlp/* -DestinationPath ./dist/yt-dlp_win.zip
+echo "yt-dlp_min.exe: $((Get-FileHash dist\yt-dlp_min.exe -Algorithm SHA256).Hash.ToLower())" >> ./dist/SHA256SUM.txt
+echo "yt-dlp_min.exe: $((Get-FileHash dist\yt-dlp_min.exe -Algorithm SHA512).Hash.ToLower())" >> ./dist/SHA512SUM.txt
+echo "yt-dlp.exe: $((Get-FileHash dist\yt-dlp.exe -Algorithm SHA256).Hash.ToLower())" >> ./dist/SHA256SUM.txt
+echo "yt-dlp.exe: $((Get-FileHash dist\yt-dlp.exe -Algorithm SHA512).Hash.ToLower())" >> ./dist/SHA512SUM.txt
+echo "yt-dlp_win.zip: $((Get-FileHash dist\yt-dlp_win.zip -Algorithm SHA256).Hash.ToLower())" >> ./dist/SHA256SUM.txt
+echo "yt-dlp_win.zip: $((Get-FileHash dist\yt-dlp_win.zip -Algorithm SHA512).Hash.ToLower())" >> ./dist/SHA512SUM.txt
+<#
+.installation
+
+uncomment and modify the path below to have the script automatically update your local executable
+
+Remove-Item -LiteralPath "$HOME\apps\youtube-dl\youtube-dl.exe" -Force
+Copy-Item -LiteralPath ".\dist\yt-dlp.exe" "$HOME\apps\youtube-dl\youtube-dl.exe"
+#>
+pause
