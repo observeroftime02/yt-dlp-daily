0de508520da7d87a0d9897b42876d17e35780a42
observeroftime01 <57168924+observeroftime01@users.noreply.github.com>
2022-10-28 18:49:43 -0700

    [build/scripts] Use Pyinstaller v.5.6.1 ci skip all

--- build-windows.ps1
+++ build-windows.ps1
@@ -2,7 +2,7 @@ Remove-Item -LiteralPath ".\dist\" -Force -Recurse
 python devscripts/update-version.py
 python devscripts/make_lazy_extractors.py
 python -m pip install --upgrade pip setuptools wheel "py2exe<0.12"
-pip install "https://observeroftime01.github.io/Pyinstaller-Builds/x86_64/pyinstaller-5.5-py3-none-any.whl" -r requirements.txt
+pip install "https://observeroftime01.github.io/Pyinstaller-Builds/x86_64/pyinstaller-5.6.1-py3-none-any.whl" -r requirements.txt
 python setup.py py2exe
 Move-Item ./dist/yt-dlp.exe ./dist/yt-dlp_min.exe
 python pyinst.py
